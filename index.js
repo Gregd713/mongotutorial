5e51842086771f9b2ad5d135508f4a5ee25fc5af
Gregory Delgado <gregd92694@yahoo.com>
2021-11-23 13:46:08 -0600

    hidden

---
+++ index.js
@@ -0,0 +1,8 @@
+const mongoose =require('mongoose');
+const config =require('config');
+
+mongoose.connect(
+    config.get('mongoURI'),
+{useNewUrlParser:true, useUnifiedTopology:true})
+.then(()=>console.log('connected to MongoDB...'))
+.catch((err)=>console.log(`Could not connect to MongoDB. Error: ${err}`));
